3b5246cfd0b46eccc5abe50a42f3366701402c39
Vlad Chernushevich <mailbox@happytodesign.com>
2013-05-12 12:06:59 +0200

    Update to v1.0.2. See changelog

--- jquery.smartscroll.js
+++ jquery.smartscroll.js
@@ -1,29 +1,38 @@
-/*-----------------------------------------------------------------------------------
-	smartScroll - v1.0.1 - 2013-05-11
-	https://github.com/happytodesign/smartScroll
+/*!
+ * jQuery smartScroll v1.0.2
+ * https://github.com/happytodesign/smartScroll
+ *
+ * Copyright 2013 happytodesign.com
+ * Released under the MIT license
+ *
+ * Date: 2013-05-11
+ */
 
-	© 2013 happytodesign.com
-	Licensed WTFPL
+
+/*-----------------------------------------------------------------------------------
+	Changelog:
+		v1.0.2
+			- Added 'scrollStart' and 'scrollEnd' callbacks
+			- Added 'relative' property to the speed option
+		v1.0.1
+			- Added 'speed', 'easing' options and 'activate' callback
 -----------------------------------------------------------------------------------*/
 
-/*-----------------------------------------------------------------------------------
-	TODO:
-		* (?) Allow for non-anchor elements
-		* Handle elements with same offset
-		* Relative scroll speed
------------------------------------------------------------------------------------*/
 
 ! function ( $, window ) {
 
 	var defaults = {
-		class: 'active',
 		offset: 0,
+
+		class: 'active',
 		selector: 'a',
 
-		speed: 1000,
+		speed: 'relative',
 		easing: 'swing',
 
-		activate: function () {}
+		activate: function () {},
+		scrollStart: function () {},
+		scrollEnd: function () {}
 	}
 
 	function smartScroll ( element, options ) {
@@ -32,50 +41,71 @@
 		this.element = element
 		this.options = $.extend( defaults, options )
 
-		this.init()
+		this._init()
 	}
 
 	smartScroll.prototype = {
 
-		init: function () {
+		refresh: function () {
+
+			this.anchors = []
+			this.offsets = {}
 
 			var that = this
 
-			this.window.on( 'scroll.smartScroll', $.proxy( this.monitor, this ) )
+			$(this.element).find( 'a[href^="#"]' ).map(function () {
+
+				var	href =  this.getAttribute( 'href' ),
+					$target = /^#\w/.test(href) && $( href ).length && $( href )
+
+				return $target && [[ href, [ $target.offset().top - that.options.offset  - 1, $target.offset().top + $target.outerHeight(true) - that.options.offset - 1 ] ]] || null
+
+			}).each(function () {
+				that.anchors.push( this[0] )
+				that.offsets[ this[0] ] = this[1]
+			})
+		},
+
+		_init: function () {
+
+			var that = this
+
+			this.window.on( 'scroll.smartScroll', $.proxy( this._monitor, this ) )
 
 			this.window.on( 'mousedown.smartScroll mousewheel.smartScroll keyup.smartScroll', function ( e ) {
 				if ( that.scrolling ) {
 					that.scrolling = false
-					$( 'body' ).stop()
+					$( 'html, body' ).stop()
 				}
 			})
 
 			this.refresh()
-			this.monitor()
-			this.smoothScroll()
+			this._monitor()
+			this._smoothScroll()
 		},
 
-		monitor: function () {
+		_monitor: function () {
 
 			if ( ! this.scrolling ) {
 
-				var active,
-					scrollTop = this.window.scrollTop() + this.options.offset + 1
+				var	active,
+					scrollTop = this.window.scrollTop()
 
 				for ( var i = this.anchors.length; i--; ) {
 
 					var anchor = this.anchors[i]
 
-					if ( scrollTop > this.offsets[ anchor ][0] && scrollTop < this.offsets[ anchor ][1] ) {
+					if ( scrollTop >= this.offsets[ anchor ][0] && scrollTop <= this.offsets[ anchor ][1] ) {
+
 						active = true
-						this.activate( anchor )
+						this._activate( anchor )
 					}
 				}
-				if ( ! active ) this.activate()
+				if ( ! active ) this._activate()
 			}
 		},
 
-		activate: function ( anchor ) {
+		_activate: function ( anchor ) {
 
 			if ( this.current != anchor ) {
 
@@ -87,46 +117,31 @@
 				undefined != this.previous && $( this.element ).find( 'a[href="' + this.previous + '"]' ).closest( selector ).removeClass( this.options.class )
 				undefined != this.current && $( this.element ).find( 'a[href="' + this.current + '"]' ).closest( selector ).addClass( this.options.class )
 
-				this.options.activate( this )
+				this.options.activate( this.current, this.previous )
 			}
 		},
 
-		refresh: function () {
+		_smoothScroll: function () {
 
-			this.anchors = []
-			this.offsets = {}
+			var that = this
 
-			var	that = this
-
-			$(this.element).find( 'a[href^="#"]' ).map(function () {
-
-				var	href =  this.getAttribute( 'href' ),
-					$target = /^#\w/.test(href) && $( href ).length && $( href )
-
-				return $target && [[ href, [ $target.offset().top, $target.offset().top + $target.outerHeight(true) ] ]] || null
-
-			}).each(function () {
-				that.anchors.push( this[0] )
-				that.offsets[ this[0] ] = this[1]
-			})
-		},
-
-		smoothScroll: function () {
-
-			var	that = this
-
-			$(this.element).on('click', 'a[href="' + this.anchors.join( '"], a[href="' ) + '"]', function ( event ) {
+			$(this.element).on( 'click', 'a[href="' + this.anchors.join( '"], a[href="' ) + '"]', function ( event ) {
 
 				that.scrolling = true
 
-				var href = this.getAttribute( 'href' )
+				this.options.scrollStart()
 
-				that.activate( href )
+				var	href = this.getAttribute( 'href' ),
+					destination = that.offsets[ href ][0] + 1
+					speed = 'relative' == that.options.speed && Math.abs( ( that.offsets[ href ][0] - that.options.offset ) - that.window.scrollTop() ) || that.options.speed
 
-				$( 'body' ).stop().animate({
-					scrollTop: that.offsets[ href ][0] - that.options.offset
-				}, that.options.speed, that.options.easing, function() {
+				that._activate( href )
+
+				$( 'html, body' ).stop().animate({
+					scrollTop: destination
+				}, speed, that.options.easing, function() {
 					that.scrolling = false
+					this.options.scrollEnd()
 				})
 
 				event.preventDefault()
